a070f9146d61da56f48594274a4f03b3a8b0757e
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-11-10 16:36:26 -0300

    Update project heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root 'cars#index'
   resources :cars
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
